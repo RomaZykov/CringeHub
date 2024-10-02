abbf19ac2bf13b74eff4247bef4d6b80f6755891
Roman Zykov <zyckoff.rom@yandex.ru>
2024-10-02 18:47:03 +0300

    Change theme

--- app/src/main/java/com/example/cringehub/ui/theme/Color.kt
+++ app/src/main/java/com/example/cringehub/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.cringehub.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryYellow = Color(0xFFF2AC30)
+val PrimaryBlack = Color(0xFF282828)
+val PrimaryGray = Color(0xFF707070)
+val PrimaryWhite = Color(0xFFFBE4CB)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SecondaryBlack = Color(0xFF0D0E10)
+val SecondaryGray = Color(0xFF737379)
--- app/src/main/java/com/example/cringehub/ui/theme/Theme.kt
+++ app/src/main/java/com/example/cringehub/ui/theme/Theme.kt
@@ -11,16 +11,16 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+//private val DarkColorScheme = darkColorScheme(
+//    primary = Purple80,
+//    secondary = PurpleGrey80,
+//    tertiary = Pink80
+//)
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = PrimaryYellow,
+    secondary = PrimaryBlack,
+    tertiary = PrimaryGray
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -46,7 +46,8 @@ fun CringeHubTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+//        darkTheme -> DarkColorScheme
+
         else -> LightColorScheme
     }
 
