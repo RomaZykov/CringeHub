ebf1ac141a2562d8326aee9ae0adfd4522d9d370
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-12-01 19:49:24 +0300

    Remove unnecessary dependencies for domain module

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,12 +1,7 @@
 plugins {
     alias(libs.plugins.cringehub.android.library)
-    alias(libs.plugins.cringehub.firebase)
 }
 
 android {
     namespace = "com.example.domain"
-}
-dependencies {
-    implementation(libs.play.services.auth)
-    implementation(libs.androidx.credentials)
 }
