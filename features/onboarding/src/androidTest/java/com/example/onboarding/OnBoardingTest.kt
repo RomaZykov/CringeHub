546b6580fe6692509dc4a00ade341aa12eda1e32
Roman Zykov <zyckoff.rom@yandex.ru>
2024-12-19 23:50:46 +0300

    Remove comments

--- features/onboarding/src/androidTest/java/com/example/onboarding/OnBoardingTest.kt
+++ features/onboarding/src/androidTest/java/com/example/onboarding/OnBoardingTest.kt
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class OnBoardingTest {
+
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
@@ -28,8 +29,6 @@ class OnBoardingTest {
 //            SemanticsActions.OnClick
 //        )?.label == label
 //    }
-
-    private val resources = composeTestRule.activity.resources
 
     @Before
     fun setUp() {
@@ -42,31 +41,15 @@ class OnBoardingTest {
 
     @Test
     fun onBoardingPager_whenDifferentSelectionUsed_showsEachPagesFromStartToEnd() {
-        composeTestRule.onNodeWithContentDescription(resources.getString(R.string.onboarding_button_cd)).assertExists()
-        composeTestRule.onNodeWithText(resources.getString(R.string.onboarding_next)).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.resources.getString(R.string.onboarding_button_cd)).assertExists()
+        composeTestRule.onNodeWithText(composeTestRule.activity.resources.getString(R.string.onboarding_next)).assertIsDisplayed()
 
         repeat(2) {
             composeTestRule.onNodeWithContentDescription("next onboarding page").performClick()
         }
-
         repeat(2) {
             composeTestRule.onNodeWithContentDescription("next onboarding page")
                 .performTouchInput { swipeRight() }
         }
-
-//        composeTestRule.onNodeWithText("Welcome").assertIsDisplayed()
-//
-//        composeRule.onNode(hasContentDescription("Description of the image")).assertExists()
-//
-//        composeTestRule .onNodeWithContentDescription(
-//                composeTestRule.activity.resources.getString(R.string.feature_foryou_loading),
-//            )
-//            .assertExists()
-//
-//        composeTestRule.onNode(hasTestTag("Players"))
-//
-//        performGesture { swipeLeft() }
-//
-//        composeRule.onNode(hasClickLabel("Moves to next screen")).assertExists()
     }
 }
