964e662735c470012c84ccd9caac4ff80868f2b8
Roman Zykov <zyckoff.rom@yandex.ru>
2024-12-24 21:48:00 +0300

    Remove screenshot tests

--- features/onboarding/build.gradle.kts
+++ features/onboarding/build.gradle.kts
@@ -2,19 +2,10 @@ plugins {
     alias(libs.plugins.cringehub.android.library)
     alias(libs.plugins.cringehub.android.library.compose)
     alias(libs.plugins.cringehub.android.feature)
-    alias(libs.plugins.compose.screenshot)
 }
 
 android {
     namespace = "com.example.feature.onboarding"
-
-    experimentalProperties["android.experimental.enableScreenshotTest"] = true
-
-    testOptions {
-        screenshotTests {
-            imageDifferenceThreshold = 0.0001f // 0.01%
-        }
-    }
 }
 
 dependencies {
