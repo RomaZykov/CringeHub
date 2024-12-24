e854f9878e41d991f6f2a238e745013800387e5c
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2024-12-24 15:19:59 +0300

    Refactor konsist

--- konsist/src/test/java/com/example/konsist/KonsistTest.kt
+++ konsist/src/test/java/com/example/konsist/KonsistTest.kt
@@ -12,7 +12,6 @@ import com.lemonappdev.konsist.api.architecture.Layer
 import com.lemonappdev.konsist.api.ext.list.functions
 import com.lemonappdev.konsist.api.ext.list.withAllParentsOf
 import com.lemonappdev.konsist.api.ext.list.withAnnotationOf
-import com.lemonappdev.konsist.api.ext.list.withName
 import com.lemonappdev.konsist.api.verify.assertFalse
 import com.lemonappdev.konsist.api.verify.assertTrue
 import kotlinx.coroutines.flow.Flow
