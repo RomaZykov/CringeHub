d735a055371e6d94e775b38dca4da8f77397d6cd
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-01-19 21:31:00 +0300

    Add custom hilt runner for tests

--- core/test/build.gradle.kts
+++ core/test/build.gradle.kts
@@ -1,7 +1,14 @@
 plugins {
     alias(libs.plugins.cringehub.android.library)
+    alias(libs.plugins.cringehub.hilt)
 }
 
 android {
     namespace = "com.example.test"
+}
+
+dependencies {
+    implementation(libs.hilt.android.testing)
+    implementation(libs.test.runner)
+    implementation(libs.test.rules)
 }
