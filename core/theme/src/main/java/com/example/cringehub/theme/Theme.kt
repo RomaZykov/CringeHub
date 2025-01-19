597ff6c08b3d048462a577b43b013d620ea47e40
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-01-19 21:33:48 +0300

    Remove dynamic feature color theme

--- core/theme/src/main/java/com/example/cringehub/theme/Theme.kt
+++ core/theme/src/main/java/com/example/cringehub/theme/Theme.kt
@@ -1,19 +1,15 @@
 package com.example.cringehub.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.platform.LocalContext
 
 private val LightColorScheme = lightColorScheme(
     primary = PrimaryYellow,
@@ -42,13 +38,7 @@ fun CringeHubTheme(
     content: @Composable () -> Unit
 ) {
     WindowInsets.safeDrawing
-    val colorScheme = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-    } else {
-        if (darkTheme) DarkColorScheme else LightColorScheme
-    }
-
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
     CompositionLocalProvider(
         LocalCustomColorScheme provides colorScheme,
         LocalCustomTypography provides CringeHubTypography(),
