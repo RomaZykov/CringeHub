6c71475df4e78e66ee783439d3314b855fee2aa6
Roman Zykov <87817259+RomaZykov@users.noreply.github.com>
2025-04-21 17:14:29 +0300

    other

--- features/adminGuideCreation/src/main/java/com/example/adminguidecreation/GuideCreationUiState.kt
+++ features/adminGuideCreation/src/main/java/com/example/adminguidecreation/GuideCreationUiState.kt
@@ -13,11 +13,11 @@ interface GuideCreationUiState {
 
     // Content descriptions for semantic provided for tests
     companion object {
-        const val EDITOR_CONTROL = "editor control"
+        const val EDITOR_CONTROL = "editor_control"
         const val TITLE = "title"
         const val CONTENT = "content"
         const val DIALOG = "dialog"
-        const val PUBLISH_BUTTON = "publish button"
+        const val PUBLISH_BUTTON = "publish_button"
         const val BACK_BUTTON = "back_button"
 
         const val BOLD_BUTTON = "bold_button"
