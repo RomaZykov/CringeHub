0aa16fd32e15d79d4a96646611226f649b427283
Roma Zykov <87817259+RomaZykov@users.noreply.github.com>
2026-03-28 18:57:05 +0300

    Delete features/adminGuideCreation/src/main/java/com/example/adminguidecreation directory

--- features/adminGuideCreation/src/main/java/com/example/adminguidecreation/navigation/BaseDraftRouteProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.example.adminGuideCreation.navigation
-
-import com.example.adminnavigation.DraftRouteProvider
-import com.example.adminnavigation.Route
-
-class BaseDraftRouteProvider : DraftRouteProvider {
-    override fun route(id: String): Route = GuideCreationRoute(id)
-}
--- features/adminGuideCreation/src/main/java/com/example/adminguidecreation/navigation/BaseGuideCreationRouteProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.example.adminGuideCreation.navigation
-
-import com.example.adminnavigation.GuideCreationRouteProvider
-import com.example.adminnavigation.Route
-
-class BaseGuideCreationRouteProvider : GuideCreationRouteProvider {
-    override fun route(): Route = GuideCreationRoute()
-}
